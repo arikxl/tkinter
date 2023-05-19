efada14586b7832ad8edd9b41f9788e261bb158c
Arik Alexandrov <arikxl@gmail.com>
2023-05-20 00:04:38 +0300

    convertor

--- demo.py
+++ demo.py
@@ -1,8 +1,11 @@
 import tkinter as tk
 from tkinter import ttk
+import ttkbootstrap as ttk
 
 def convert():
-
+    mile_input = entry_int.get()
+    km_output = mile_input* 1.61
+    output_string.set(km_output)
 
 window = tk.Tk()
 window.title = ('Demo')
@@ -12,14 +15,15 @@ title_label = ttk.Label(master= window, text= 'Miles to Kilometers', font= 'Cali
 title_label.pack()
 
 input_frame = ttk.Frame(master= window)
-entryInt = tk.IntVar()
-entry = ttk.Entry(master= input_frame, textvariable= entryInt)
+entry_int = tk.IntVar()
+entry = ttk.Entry(master=input_frame, textvariable=entry_int)
 button = ttk.Button(master= input_frame, text= 'Convert', command=convert)
 entry.pack(side='left', padx= 10)
 button.pack(side='left')
 input_frame.pack(pady = 10)
 
-output_label = ttk.Label(master=window, text='Output', font='Calibri 24')
+output_string= tk.StringVar()
+output_label = ttk.Label(master=window, text='Output', font='Calibri 24', textvariable= output_string)
 output_label.pack(pady=5)
 
 window.mainloop()
